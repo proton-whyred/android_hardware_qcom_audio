b105a8d23810beb46f1d7ba8dfdb0ae41dd763e5
Gangadhar S <gangadha@codeaurora.org>
2019-12-12 14:39:13 +0530

    hal: Add support for hdmi passthrough
    - Update EAC3 and AC3 format to fall under
      compress passthrough apptype profile.
    - Set offload passthrough property to one.
    
    Change-Id: If8d2774b626c736349cf27a4ced47ffaea2badd6

--- configs/msm8998/msm8998.mk
+++ configs/msm8998/msm8998.mk
@@ -182,7 +182,7 @@ vendor.audio.offload.multiple.enabled=false
 
 #Disable Compress passthrough playback
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.audio.offload.passthrough=false
+vendor.audio.offload.passthrough=true
 
 #Disable surround sound recording
 PRODUCT_PROPERTY_OVERRIDES += \
